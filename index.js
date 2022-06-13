5d4fbdd531b4484b43c2f82bd54888f5ac4a89f8
Peyton Harrison <peyton006@hotmail.com>
2022-06-13 10:53:01 -0500

    commit tree 244777cc6b486580a725f6a1371262ce3992383f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const cards = [];
+
+function writeCards(cards, event) {
+    let newCards = [];
+    for (let i = 0; i < cards.length; i++) {
+         newCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return newCards;
+
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
